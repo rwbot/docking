5c4f7065577320471bd4bc3d2831c036aabe607c
rwbot <rwbotx@gmail.com>
2019-11-17 18:53:49 -0500

    feat: add RansacLine, SeparateClouds, ClusterLines, VoxelGrid

--- include/docking/SegmentLine.h
+++ include/docking/SegmentLine.h
@@ -1,6 +1,6 @@
 #ifndef SEGMENTLINE_H_
 #define SEGMENTLINE_H_
-
+#include <ros/ros.h>
 #include <pcl/io/io.h>
 #include <pcl/common/common.h>
 #include <pcl/pcl_base.h>
@@ -19,24 +19,43 @@
 #include <vector>
 #include <ctime>
 #include <chrono>
-// typedef pcl_ros::SACSegmentation SACSegmentation ;
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
 //namespace docking{
 
   template<typename PointT>
-  class SegmentLine : public pcl::PCLBase<PointT>
+  class SegmentLine
   {
 
     public:
-      using pcl::PCLBase<PointT>::input_;
       SegmentLine(){}
       ~SegmentLine(){}
 
- ///////////////// BEGIN RANSAC LINE /////////////////
-      std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> RansacLine(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceThreshold)
+      typename pcl::PointCloud<PointT>::Ptr input;
+      typename pcl::PointCloud<PointT>::Ptr output;
+
+      void setInputCloud(typename pcl::PointCloud<PointT>::Ptr in)
+      {
+        input = in;
+      }
+
+      void setParams(typename pcl::PointCloud<PointT>::Ptr in)
+      {
+        input = in;
+      }
+
+      ///////////////// BEGIN RANSAC LINE /////////////////
+      /// \brief RansacLine
+      /// \param inputCloudPtr
+      /// \param maxIterations
+      /// \param distanceThreshold
+      /// \return lines
+      ///
+     std::vector<typename pcl::PointCloud<PointT>::Ptr> RansacLine(typename pcl::PointCloud<PointT>::Ptr inputCloudPtr, int maxIterations, float distanceThreshold)
+//     std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> RansacLine(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceThreshold)
     {
-      auto startTime = std::chrono::steady_clock::now();
+//      auto startTime = std::chrono::steady_clock::now();
+      int minInliers = 10;
       pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
       pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
       //Create the segmentation object
@@ -46,36 +65,148 @@ typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
       seg.setMethodType(pcl::SAC_RANSAC);
       seg.setMaxIterations(maxIterations);
       seg.setDistanceThreshold(distanceThreshold);
-      //Segment the largest planar component from the input cloud
-//      seg.setInputCloud(cloud);
-      seg.setInputCloud(input_);
-      seg.segment(*inliers, *coefficients);
-      if (inliers->indices.size() == 0) { std::cout << "Could not estimate a line model for the given dataset." << std::endl; }
-      //
-      auto endTime = std::chrono::steady_clock::now();
-      auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
-      std::cout << "RANSAC took " << elapsedTime.count() << " microseconds" << std::endl;
 
-//                std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> segResult = SeparateClouds(inliers, cloud);
-      std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> line(coefficients, inliers);
-        return line;
+      std::vector<typename pcl::PointCloud<PointT>::Ptr > lines;
+      std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> segResult;
+      typename pcl::PointCloud<PointT>::Ptr inCloudPtr, outCloudPtr;
+      inCloudPtr = inputCloudPtr;
+      int i = 1;
+      while(inCloudPtr->size() != 0){
+        std::cout <<  std::endl <<"Iteration # " << i++ << std::endl;
+        if (i > 100) { break; }
+        //Segment the largest line component from the input cloud
+        seg.setInputCloud(inCloudPtr);
+        seg.segment(*inliers, *coefficients);
+
+        segResult = SeparateClouds(inliers, inCloudPtr);
+
+        typename pcl::PointCloud<PointT>::Ptr line (new pcl::PointCloud<PointT>);
+        line = segResult.first;
+        line->width = inliers->indices.size();
+        line->height = 1;
+        line->is_dense = true;
+        lines.push_back(line);
+        inCloudPtr = segResult.second;
+        if (inliers->indices.size() <= minInliers) {
+          std::cout << std::endl << "Less than " << minInliers << " inliers left" << std::endl;
+          break;
+        }
+      }
+
+      std::cout << "RANSAC: " << lines.size() << " lines found" << std::endl;
+//      auto endTime = std::chrono::steady_clock::now();
+//      auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+//      std::cout << "RANSAC took " << elapsedTime.count() << " microseconds" << std::endl;
+
+//      std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> line(coefficients, inliers);
+      return lines;
       }
 ///////////////// END RANSAC LINE /////////////////
+
+     ///////////////// BEGIN SEPARATE CLOUDS /////////////////
+     /// \brief SeparateClouds
+     /// \param inliers
+     /// \param cloud
+     /// \return
+     ///
+      std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> SeparateClouds(pcl::PointIndices::Ptr inliers, typename pcl::PointCloud<PointT>::Ptr cloud)
+      {
+          typename pcl::PointCloud<PointT>::Ptr inCloud (new pcl::PointCloud<PointT>());
+          typename pcl::PointCloud<PointT>::Ptr outCloud (new pcl::PointCloud<PointT>());
+
+          // Obtain the inlier point cloud by copying only inlier indices to the planeCloud
+          // for(int index : inliers->indices)
+          //     inCloud->points.push_back(cloud->points[index]);
+          // ****** ^ does the same as below for the inliers, cuz we already have the inlier indices******
+          // Create extract object
+          pcl::ExtractIndices<PointT> extract;
+          extract.setInputCloud (cloud);
+          extract.setIndices (inliers);
+
+          // Get plane cloud (inliers)
+          extract.setNegative (false);    // Extract the inliers, not outliers
+          extract.filter (*inCloud);   //Output cloud
+
+          // Get obstacle cloud (outliers)
+          extract.setNegative (true);     // Extract the outliers, not inlierss
+          extract.filter (*outCloud);    //Output cloud
+          std::cerr << "Inliers: " << inCloud->width * inCloud->height << " points " << " Outliers: " << outCloud->width * outCloud->height << " points" << std::endl;
+
+          std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> segResult(inCloud, outCloud);
+          return segResult;
+      }
+///////////////// END SEPARATE CLOUDS /////////////////
+
+      ///////////////// BEGIN CLUSTER LINES /////////////////
+      /// \brief ClusterLines
+      /// \param cloud
+      /// \param clusterTolerance
+      /// \param minSize
+      /// \param maxSize
+      /// \return
+      ///
+      typename pcl::PointCloud<PointT>::Ptr ClusterLines(typename pcl::PointCloud<PointT>::Ptr cloud, double clusterTolerance, int minSize, int maxSize)
+      {
+        typename pcl::PointCloud<PointT>::Ptr inCloud (new pcl::PointCloud<PointT>());
+        typename pcl::PointCloud<PointT>::Ptr outCloud (new pcl::PointCloud<PointT>());
+        typename pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
+        tree->setInputCloud (cloud);
+        std::vector<pcl::PointIndices> cluster_indices;
+        pcl::EuclideanClusterExtraction<PointT> ec;
+        ec.setClusterTolerance (clusterTolerance);
+        ec.setMinClusterSize (minSize);
+        ec.setMaxClusterSize (maxSize);
+        ec.setSearchMethod (tree);
+        ec.setInputCloud (cloud);
+        ec.extract(cluster_indices);
+
+        std::vector<typename pcl::PointCloud<PointT>::Ptr > clusters;
+        for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+        {
+          typename pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>);
+          for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
+            cloud_cluster->points.push_back(cloud->points[*pit]);
+
+          cloud_cluster->width = cloud_cluster->points.size ();
+          cloud_cluster->height = 1;
+          cloud_cluster->is_dense = true;
+          std::cout << "Cluster has " << cloud_cluster->points.size() << " points.\n";
+          // Add current cluster to list of clusters
+          clusters.push_back(cloud_cluster);
+        }
+      }
+///////////////// END CLUSTER LINES /////////////////
+
+      ///////////////// BEGIN VOXEL GRID /////////////////
+      /// \brief VoxelGrid
+      /// \param inCloudPtr
+      /// \param leafSize
+      /// \return
+      ///
+
+      typename pcl::PointCloud<PointT>::Ptr VoxelGrid(typename pcl::PointCloud<PointT>::Ptr inCloudPtr, float leafSize)
+      {
+        typename pcl::PointCloud<PointT>::Ptr filteredCloudPtr (new pcl::PointCloud<PointT>());
+        // Perform the actual filtering
+        pcl::VoxelGrid<PointT> voxel;
+        voxel.setInputCloud(inCloudPtr);
+        voxel.setLeafSize(leafSize, leafSize, leafSize);
+        voxel.filter(*filteredCloudPtr);
+
+
+      }
+///////////////// END VOXEL GRID /////////////////
+
   };
 
 //}
 
-//template std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> docking::SegmentLine<pcl::PointXYZ>::RansacLine();
-//<pcl::PointCloud<pcl::PointXYZ>::Ptr, int, float>(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceThreshold);
 
 //#include <docking/impl/SegmentLine.hpp>
-
-
 //#define PCL_INSTANTIATE_SegmentLine(T) template class PCL_EXPORTS pcl::SegmentLine<T>;
   template class SegmentLine<pcl::PointXYZI>;
   template class SegmentLine<pcl::PointXYZ>;
   template class SegmentLine<pcl::PointXYZRGB>;
   template class SegmentLine<pcl::PointXYZRGBA>;
-//  template class SegmentLine<pcl::PointCloud<pcl::PointXYZ>>;
 
 #endif /*"SEGMENTLINE_H_"*/
--- src/line_detection.cpp
+++ src/line_detection.cpp
@@ -3,79 +3,80 @@
 #include <docking/SegmentLine.h>
 //#include <docking/impl/SegmentLine.hpp>
 //#include "SegmentLine.cpp"
-// Include pcl
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+// Include PointCloud2 message
+#include <sensor_msgs/PointCloud2.h>
+// Include pcl
 #include <pcl/point_cloud.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/sample_consensus/ransac.h>
 //#include <pcl/sample_consensus/sac_model_line.h>
-// Include PointCloud2 message
-#include <sensor_msgs/PointCloud2.h>
+
 
 // Topics
-static const std::string IMAGE_TOPIC = "/cloud_pc2";
-static const std::string PUBLISH_TOPIC = "/pcl/points";
+static const std::string INPUT_CLOUD = "/cloud";
+static const std::string PUBLISH_TOPIC = "/dockLines";
+
+typedef pcl::PointXYZI PointT;
 
 // ROS Publisher
 ros::Publisher pub;
-
-void cloud_cb(const sensor_msgs::PointCloud2ConstPtr &cloud_msg)
+int max_iter;
+float dist_thresh;
+void cloud_cb(const sensor_msgs::PointCloud2ConstPtr &msg)
+//void cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &msg)
 {
     // Container for original & filtered data
-    pcl::PCLPointCloud2 *cloud_pc2 = new pcl::PCLPointCloud2;
-    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud_pc2);
-    pcl::PCLPointCloud2 cloud_filtered;
+    /*
+    * CONVERT POINTCLOUD ROS->PCL
+    */
+    pcl::PointCloud<PointT> cloud;
+    pcl::fromROSMsg (*msg, cloud);
+    pcl::PointCloud<PointT>::Ptr cloud_ptr (new pcl::PointCloud<PointT> (cloud));
+    pcl::PointCloud<PointT>::Ptr cloud_voxel_filtered (new pcl::PointCloud<PointT> ());
 
-    // Convert to PCL data type
-    pcl_conversions::toPCL(*cloud_msg, *cloud_pc2);
-
-    // Perform the actual filtering
-    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-    sor.setInputCloud(cloudPtr);
-    sor.setLeafSize(0.001f, 0.001f, 0.001f);
-    sor.filter(cloud_filtered);
-
-
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pcxyz(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromPCLPointCloud2(*cloud_pc2, *cloud_pcxyz);
-
-    SegmentLine<pcl::PointXYZ> *segmentLine = new SegmentLine<pcl::PointXYZ>();
-    segmentLine->setInputCloud(cloud_pcxyz);
-
-//    docking::SegmentLine<pcl::PointXYZI> segmentLine (new docking::SegmentLine<pcl::PointXYZI>);
-    //docking::SegmentLine<pcl::PointXYZI> *segmentLine = new docking::SegmentLine<pcl::PointXYZI>();
+    SegmentLine<PointT> *segmentLine = new SegmentLine<PointT>();
+    segmentLine->setInputCloud(cloud_ptr);
+    ROS_INFO_STREAM("Distance Threshold: " << dist_thresh);
+    std::vector<typename pcl::PointCloud<PointT>::Ptr> lines = segmentLine->RansacLine(cloud_ptr, max_iter, dist_thresh);
 //    std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> line =
 //    segmentLine->RansacLine();
 
-    std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> line = segmentLine->RansacLine(cloud_pcxyz, 1000, 0.01f);
+//    std::pair<pcl::ModelCoefficients::Ptr, pcl::PointIndices::Ptr> line = segmentLine->RansacLine(*msg, 1000, 0.01f);
 
-    // Convert to ROS data type
-    sensor_msgs::PointCloud2 output;
-//    output = segmentLine->input_ ;
-//    pcl_conversions::moveFromPCL(cloud_pcxyz, output);
-//    pcl_conversions::
-//    pcl::toPCLPointCloud2(cloud_pcxyz, output);
-//    pcl_conversions::moveFromPCL(cloud_filtered, output);
-    // Publish the data
-    pub.publish(output);
+    /* ========================================
+     * CONVERT POINTCLOUD PCL->ROS
+     * PUBLISH CLOUD
+     * ========================================*/
+    sensor_msgs::PointCloud2::Ptr pc2_cloud (new sensor_msgs::PointCloud2);
+    pcl::toROSMsg(*lines.at(1), *pc2_cloud);
+//    pc2_cloud->header.frame_id=world_frame;
+//    pc2_cloud->header.stamp=ros::Time::now();
+    pub.publish(pc2_cloud);
 }
 
 int main(int argc, char **argv)
 {
-    // Initialize the ROS Node "roscpp_pcl_example"
-    ros::init(argc, argv, "roscpp_pcl_example");
+    // Initialize the ROS Node "line_detection"
+    ros::init(argc, argv, "line_detection");
     ros::NodeHandle nh;
+
+    max_iter = nh.param<int>("max_iterations", 50);
+    dist_thresh = nh.param<float>("distance_threshold", 0.01);
 
     // Print "Hello" message with node name to the terminal and ROS log file
     ROS_INFO_STREAM("Hello from ROS Node: " << ros::this_node::getName());
 
-    // Create a ROS Subscriber to IMAGE_TOPIC with a queue_size of 1 and a callback function to cloud_cb
-    ros::Subscriber sub = nh.subscribe(IMAGE_TOPIC, 1, cloud_cb);
+    // Create a ROS Subscriber to INPUT_CLOUD with a queue_size of 1 and a callback function to cloud_cb
+    ros::Subscriber sub = nh.subscribe(INPUT_CLOUD, 1, cloud_cb);
 
     // Create a ROS publisher to PUBLISH_TOPIC with a queue_size of 1
-    pub = nh.advertise<sensor_msgs::PointCloud2>(PUBLISH_TOPIC, 1);
+//    pub = nh.advertise<sensor_msgs::PointCloud2>(PUBLISH_TOPIC, 1);
+    // create a templated publisher
+    pub = nh.advertise<pcl::PointCloud<PointT>>(PUBLISH_TOPIC, 1);
 
     // Spin
     ros::spin();
